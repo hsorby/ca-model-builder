b5a7510eb8b3df0904d7586b7988cdd76a335ddf
Hugh Sorby <h.sorby@auckland.ac.nz>
2025-10-28 21:55:42 +1300

    Stop vite from preloading vue3-libcellml.js this seems to cause a loading error.

--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,20 @@ export default defineConfig({
   server: {
     fs: {
       // Allow serving files from one level up to the project root
-      allow: [
-        "..",
-        "/Users/hsor001/Projects/cellml/javascript/build-libcellml-release/src/bindings/javascript/",
-      ],
+      // When using yarn link you may need to add additional allowed paths here.
+      // '..' for serving index.html from the parent folder.
+      // allow: [
+        // "..",
+        // "/some/path/to/wasm/files",
+      // ],
+    },
+  },
+  optimizeDeps: {
+    // Exclude the wasm-based library from pre-bundling
+    exclude: ['vue3-libcellml.js'],
+    // You may also need this target setting from your working config
+    esbuildOptions: {
+      target: 'es2020',
     },
   },
 })
