e137f45d3d7203f59e6ab9af0632cef6061ff75a
Hugh Sorby <h.sorby@auckland.ac.nz>
2025-12-18 11:05:50 +1300

    Set types to buttons in upload config dialog.

--- src/components/UploadConfigDialog.vue
+++ src/components/UploadConfigDialog.vue
@@ -7,12 +7,12 @@
     @closed="resetForm"
     @update:model-value="closeDialog"
   >
-    <el-form 
-      label-position="top">
-      <el-form-item 
-      label="Select vessel array file"
-      :required="true"
-      inline-message>
+    <el-form label-position="top">
+      <el-form-item
+        label="Select vessel array file"
+        :required="true"
+        inline-message
+      >
         <div class="upload-row">
           <el-upload
             :auto-upload="false"
@@ -25,63 +25,61 @@
               placeholder="No file selected"
               readonly
             >
-              <template #append>
-                <el-button>Browse</el-button> 
-              </template>
             </el-input>
+            <el-button type="success">Browse</el-button>
           </el-upload>
           <el-icon v-if="vesselArrayValid" color="green">
             <Check />
           </el-icon>
         </div>
       </el-form-item>
-      <el-form-item 
-      label="Select module configuration file"
-      :required="true"
-      inline-message>
-      <div class="upload-row">
-        <el-upload
-          :auto-upload="false"
-          :on-change="handleModuleConfig"
-          :show-file-list="false"
-          :accept="'.json'"
-        >
-          <el-input
-            :model-value="moduleConfigName"
-            placeholder="No file selected"
-            readonly
-            class="file-input"
-          > 
-            <template #append>
-              <el-button>Browse</el-button> 
-            </template>
-        </el-input>
-        </el-upload>
+      <el-form-item
+        label="Select module configuration file"
+        :required="true"
+        inline-message
+      >
+        <div class="upload-row">
+          <el-upload
+            :auto-upload="false"
+            :on-change="handleModuleConfig"
+            :show-file-list="false"
+            :accept="'.json'"
+          >
+            <el-input
+              :model-value="moduleConfigName"
+              placeholder="No file selected"
+              readonly
+              class="file-input"
+            >
+            </el-input>
+            <el-button type="success">Browse</el-button>
+          </el-upload>
 
-        <el-icon v-if="moduleConfigValid" color="green">
-          <Check />
-        </el-icon>
-      </div>
-    </el-form-item>
+          <el-icon v-if="moduleConfigValid" color="green">
+            <Check />
+          </el-icon>
+        </div>
+      </el-form-item>
     </el-form>
 
-  <template #footer>
-    <span class="dialog-footer">
+    <template #footer>
+      <span class="dialog-footer">
         <el-button @click="closeDialog">Cancel</el-button>
-        <el-button 
-        type="primary" 
-        @click="handleConfirm"
-        :disabled="!vesselArrayValid || !moduleConfigValid">
-        Confirm 
-      </el-button>
+        <el-button
+          type="primary"
+          @click="handleConfirm"
+          :disabled="!vesselArrayValid || !moduleConfigValid"
+        >
+          Confirm
+        </el-button>
       </span>
-  </template> 
+    </template>
   </el-dialog>
 </template>
 
 <script setup>
 import { computed, reactive, ref } from 'vue'
-import { ElNotification } from "element-plus"
+import { ElNotification } from 'element-plus'
 import { Check } from '@element-plus/icons-vue'
 import Papa from 'papaparse'
 
@@ -90,12 +88,12 @@ const props = defineProps({
   modelValue: {
     type: Boolean,
     default: false,
-  }
+  },
 })
 
 const emit = defineEmits([
-  "update:modelValue", // Required for v-model
-  "confirm", // Emits the new data
+  'update:modelValue', // Required for v-model
+  'confirm', // Emits the new data
 ])
 
 function resetForm() {
@@ -106,67 +104,66 @@ function resetForm() {
 }
 
 function closeDialog() {
-  emit("update:modelValue", false)
+  emit('update:modelValue', false)
 }
 
 function handleConfirm() {
-
-  emit("confirm", {
-      vesselArray: vesselArray.data,
-      moduleConfig: moduleConfig.data,
-    })
+  emit('confirm', {
+    vesselArray: vesselArray.data,
+    moduleConfig: moduleConfig.data,
+  })
 
   closeDialog()
 }
 
 const vesselArray = reactive({
-  data:[]
+  data: [],
 })
 
 const moduleConfig = reactive({
-  data:[]
+  data: [],
 })
 
 const handleVesselArray = (file) => {
   vesselArrayName.value = file.name
   vesselArrayValid.value = true
-  
+
   Papa.parse(file.raw, {
-  header: true, // Converts row 1 to object keys
-  skipEmptyLines: true,
-  
-  complete: (results) => {
-    try{
-      // Error handle - confirm that the file loaded has the required columns (i.e., is a valid vessel array file)
-      if (!(results.data.length > 0 &&
-        "name" in results.data[0] &&
-        "BC_type" in results.data[0] &&
-        "vessel_type" in results.data[0] &&
-        "inp_vessels" in results.data[0] &&
-        "out_vessels" in results.data[0])
-      ) {
-        throw new Error("Invalid vessel array file format.")
-      } 
-      // results.data will be an array of objects
-      // e.g., [{ name: 'a', BC_type: 'nn', vessel_type: 'a', inp_vessels: space separated list, out_vessels: space separated list }, { object 2 }]
-      vesselArray.data = results.data
+    header: true, // Converts row 1 to object keys
+    skipEmptyLines: true,
 
-    } catch (error) {
-      ElNotification.error(error.message),
-      vesselArrayValid.value = false 
-      vesselArrayName.value = null
-    }
+    complete: (results) => {
+      try {
+        // Error handle - confirm that the file loaded has the required columns (i.e., is a valid vessel array file)
+        if (
+          !(
+            results.data.length > 0 &&
+            'name' in results.data[0] &&
+            'BC_type' in results.data[0] &&
+            'vessel_type' in results.data[0] &&
+            'inp_vessels' in results.data[0] &&
+            'out_vessels' in results.data[0]
+          )
+        ) {
+          throw new Error('Invalid vessel array file format.')
+        }
+        // results.data will be an array of objects
+        // e.g., [{ name: 'a', BC_type: 'nn', vessel_type: 'a', inp_vessels: space separated list, out_vessels: space separated list }, { object 2 }]
+        vesselArray.data = results.data
+      } catch (error) {
+        ElNotification.error(error.message), (vesselArrayValid.value = false)
+        vesselArrayName.value = null
+      }
+    },
 
-  },
-
-  error: (err) => {
-    ElNotification.error({
-      title: "CSV Parse Error",
-      message: err.message,
-    }),
-    vesselArrayValid.value = false 
-  }
-})
+    error: (err) => {
+      ElNotification.error({
+        title: 'CSV Parse Error',
+        message: err.message,
+      }),
+        (vesselArrayValid.value = false)
+    },
+  })
 }
 
 const handleModuleConfig = (file) => {
@@ -179,25 +176,26 @@ const handleModuleConfig = (file) => {
     try {
       const parsed = JSON.parse(reader.result)
 
-
-      if (!(parsed.length > 0 &&
-        "entrance_ports" in parsed[0] &&
-        "exit_ports" in parsed[0] &&
-        "general_ports" in parsed[0] &&
-        "BC_type" in parsed[0] &&
-        "vessel_type" in parsed[0] &&
-        "module_format" in parsed[0] &&
-        "module_file" in parsed[0] &&
-        "module_type" in parsed[0])
+      if (
+        !(
+          parsed.length > 0 &&
+          'entrance_ports' in parsed[0] &&
+          'exit_ports' in parsed[0] &&
+          'general_ports' in parsed[0] &&
+          'BC_type' in parsed[0] &&
+          'vessel_type' in parsed[0] &&
+          'module_format' in parsed[0] &&
+          'module_file' in parsed[0] &&
+          'module_type' in parsed[0]
+        )
       ) {
-        throw new Error("Invalid module configuration file format.")
-      } 
+        throw new Error('Invalid module configuration file format.')
+      }
 
       moduleConfig.data = parsed
       moduleConfigValid.value = true
     } catch (error) {
-      ElNotification.error(error.message),
-      moduleConfigValid.value = false 
+      ElNotification.error(error.message), (moduleConfigValid.value = false)
       moduleConfigName.value = null
     }
   }
@@ -207,25 +205,24 @@ const vesselArrayName = ref(null)
 const moduleConfigName = ref(null)
 const vesselArrayValid = ref(false)
 const moduleConfigValid = ref(false)
-
 </script>
 
 <style scoped>
-  .file-input {
-    flex: 1;
-  }
+.file-input {
+  flex: 1;
+}
 
-  .file-input input {
-    cursor: pointer;
-    white-space: nowrap;
-    text-overflow: ellipsis;
-    overflow: hidden;
-  }
+.file-input input {
+  cursor: pointer;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+}
 
-  .upload-row {
-    display: flex;
-    align-items: center;
-    gap: 12px;
-    margin-bottom: 20px;
-  }
+.upload-row {
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  margin-bottom: 20px;
+}
 </style>
